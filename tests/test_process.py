851afc39766f8ef70ec08a3309c9d84ba11ffe03
Mario Angarita <mf.angarita52@gmail.com>
2025-10-20 13:15:34 -0800

    add more tests and token

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,34 +1,56 @@
 import subprocess
 from pathlib import Path
 
-from hyp3_mintpy.process import rename_products, write_cfg
+import opensarlab_lib as osl
+from hyp3_mintpy import util
+from hyp3_mintpy.process import rename_products, set_same_frame, write_cfg
 
 
-def test_rename_products(script_runner):
-    Path('test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000').mkdir(parents=True)
-    with Path(
-        'test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000.txt'
-    ).open('w') as test:
+def test_rename_products():
+    Path(f"test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000").mkdir(parents = True)
+    with Path(f"test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000.txt").open('w') as test:
         test.write('S1_000000_IW1_00000000T000000_VV_AAAA-BURST')
 
-    rename_products('test')
-    folder = Path('test/S1_000000_IW1_00000000_00000000_VV_INT80_0000')
-    txt = folder / 'S1_000000_IW1_00000000_00000000_VV_INT80_0000.txt'
+    rename_products("test")
+    folder = Path("test/S1_000000_IW1_00000000_00000000_VV_INT80_0000")
+    txt = folder / "S1_000000_IW1_00000000_00000000_VV_INT80_0000.txt"
 
     assert folder.is_dir()
     assert txt.exists()
 
-    subprocess.call('rm -rf test', shell=True)
+    subprocess.call(f"rm -rf test", shell=True)
 
 
-def test_write_cfg(script_runner):
+def test_set_same_frame(test_data_directory):
+    data = test_data_directory
+
+    Path(f"test/test").mkdir(parents = True)
+    sdata = str(data)
+    test = Path('test/test')
+    stest = str(test)
+    subprocess.call(f'cp {sdata}/*.tif {stest}/', shell = True)
+
+    set_same_frame("test", wgs84=True)
+
+    extent_unw = osl.get_common_coverage_extents([test / 'test_unw_phase.tif'])
+    extent_mask = osl.get_common_coverage_extents([test / 'test_water_mask.tif'])
+
+    assert extent_unw == extent_mask
+    
+    epsg_unw = util.get_epsg(str(test / 'test_unw_phase.tif'))
+    epsg_mask = util.get_epsg(str(test / 'test_water_mask.tif'))
+
+    assert epsg_unw == epsg_mask
+
+
+def test_write_cfg():
     job_name = 'test_job'
     min_coherence = '0.5'
     write_cfg(job_name, min_coherence)
 
-    assert Path(f'{job_name}/MintPy/{job_name}.txt').exists()
+    assert Path(f"{job_name}/MintPy/{job_name}.txt").exists()
 
-    with Path(f'{job_name}/MintPy/{job_name}.txt').open() as cfg:
+    with Path(f"{job_name}/MintPy/{job_name}.txt").open() as cfg:
         lines = cfg.readlines()
 
     minCoh = 0
@@ -38,4 +60,4 @@ def test_write_cfg(script_runner):
 
     assert minCoh == float(min_coherence)
 
-    subprocess.call(f'rm -rf {job_name}', shell=True)
+    subprocess.call(f"rm -rf {job_name}", shell=True)
