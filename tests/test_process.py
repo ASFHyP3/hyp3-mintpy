1a7d6ede088d5344f3aace6f37e6bcd0e2742c13
Mario Angarita <mf.angarita52@gmail.com>
2025-10-20 11:19:15 -0800

    add tests

---
+++ tests/test_process.py
@@ -0,0 +1,40 @@
+import subprocess
+from pathlib import Path
+
+from hyp3_mintpy.process import rename_products
+from hyp3_mintpy.process import write_cfg
+
+
+def test_rename_products(script_runner):
+    Path(f"test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000").mkdir(parents = True)
+    with Path(f"test/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000/S1A_000_W000_0_N00_0_E000_0_N00_0_00000000_00000000_VV_INT80_0000.txt").open('w') as test:
+        test.write('S1_000000_IW1_00000000T000000_VV_AAAA-BURST')
+
+    rename_products("test")
+    folder = Path("test/S1_000000_IW1_00000000_00000000_VV_INT80_0000")
+    txt = folder / "S1_000000_IW1_00000000_00000000_VV_INT80_0000.txt"
+
+    assert folder.is_dir()
+    assert txt.exists()
+
+    subprocess.call(f"rm -rf test", shell=True)
+
+
+def test_write_cfg(script_runner):
+    job_name = 'test_job'
+    min_coherence = '0.5'
+    write_cfg(job_name, min_coherence)
+
+    assert Path(f"{job_name}/MintPy/{job_name}.txt").exists()
+
+    with Path(f"{job_name}/MintPy/{job_name}.txt").open() as cfg:
+        lines = cfg.readlines()
+
+    minCoh = 0
+    for line in lines:
+        if 'minCoherence' in line:
+            minCoh = float(line.split('=')[1])
+
+    assert minCoh == float(min_coherence)
+
+    subprocess.call(f"rm -rf {job_name}", shell=True)
