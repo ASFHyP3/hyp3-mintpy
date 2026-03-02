c7055e36d3efbb21543951ca2c38c5b63a8480df
Mario Angarita <mf.angarita52@gmail.com>
2026-03-02 08:58:23 -0900

    switch priority for bucket

--- src/hyp3_mintpy/process.py
+++ src/hyp3_mintpy/process.py
@@ -331,12 +331,13 @@ def process_mintpy(
     elif project_name is not None and input_prefix is not None:
         warnings.warn('Both job name and prefix were given. You should give just one. Using job name...')
 
-    if project_name is not None:
-        output_name = download_job_pairs(project_name, start, end)
-    else:
+    if input_bucket is not None:
         if input_prefix is None:
             input_prefix = ''
         output_name = download_bucket_pairs(str(input_bucket), input_prefix, start, end)
+    else:
+        output_name = download_job_pairs(project_name, start, end)
+
     set_same_frame(output_name, wgs84=True)
 
     write_cfg(output_name, str(min_coherence))
